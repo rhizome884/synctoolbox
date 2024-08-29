325a012061dc164f1b39f40dbd1fdc6868fa07ea
rhizome884 <rhizome884@gmail.com>
2024-08-29 10:15:33 +0100

    csv column index

--- jams/csv_to_jams_verbose.py
+++ jams/csv_to_jams_verbose.py
@@ -5,6 +5,17 @@ import csv
 """
 Create a function and script that converts time warped symbolic music data
 in CSV format to the JAMS format.
+
+CSV Column Index:
+    0: start (note onset time is seconds)
+    1: duration (note duration in seconds)
+    2: pitch (MIDI note number, 0-127)
+    3: velocity (MIDI velocity number, 0-127)
+    4: instrument (guitar)
+    5: fret (fret number -- 0-21, or go higher? dead notes?)
+    6: string (1-6 for a standard guitar)
+    7: tuning (MIDI note number that represents open string tuning)
+    8: effects ('nan' or a list containing dictionaries for various effects) 
 """
 
 def csv_to_jams(incsv, inwav, outjams):
@@ -25,6 +36,8 @@ def csv_to_jams(incsv, inwav, outjams):
     jam.file_metadata.duration = track_duration
 
     # Construct annotation record
+    for i in range(1,7):
+        print(i)
     note_tab = jams.Annotation(namespace='note_tab')
 
     # Add metadata to annotation (JUST USE EXAMPLE METADATA ANNOTATION FOR NOW)
